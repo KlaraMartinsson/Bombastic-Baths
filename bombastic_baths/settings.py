70fc62e5013e227ee3c1c52e707aef403bb95453
KlaraM <klara.ma@outlook.com>
2024-03-25 17:09:25 +0100

    Add allowed host

--- bombastic_baths/settings.py
+++ bombastic_baths/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n=d4vwb+_blonp-3zju-f%j80kc41n$s8g@^qibbpvt)37es$h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    
 ]
 
 MIDDLEWARE = [
