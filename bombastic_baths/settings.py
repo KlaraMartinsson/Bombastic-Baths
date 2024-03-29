ac943128e0b92fdacb439bf89d080c4c3620afe7
KlaraM <klara.ma@outlook.com>
2024-03-29 14:14:11 +0100

    Add new secret key

--- bombastic_baths/settings.py
+++ bombastic_baths/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import os
 import dj_database_url
+
 if os.path.isfile('env.py'):
     import env
 
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('69874EC133295')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
