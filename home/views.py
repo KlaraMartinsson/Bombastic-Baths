bb6eb2ec1bff0f4b527cb689245f9a8547646acf
KlaraM <klara.ma@outlook.com>
2024-03-30 20:57:54 +0100

    Setup view and urls for products

--- home/views.py
+++ home/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def home_page(request):
-    return render (request, "home/index.html")
+    return render (request, 'home/index.html')
 
 def our_story(request):
-    return render (request, "home/about.html")
+    return render (request, 'home/about.html')
 
 def privacy_policy(request):
-    return render (request, "home/privacypolicy.html")
+    return render (request, 'home/privacypolicy.html')
 
 def faq(request):
-    return render (request, "home/faq.html")
+    return render (request, 'home/faq.html')
