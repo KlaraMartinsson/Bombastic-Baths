bc865ebb30fac9e44a3d657615e47dc4ed0887f3
KlaraM <klara.ma@outlook.com>
2024-04-01 13:44:25 +0200

    Add a category filter for products

--- products/views.py
+++ products/views.py
@@ -5,6 +5,11 @@ from .models import Product
 def all_products(request):
 
     products = Product.objects.all()
+    category = None
+    if 'category' in request.GET:
+        category = request.GET['category']
+        products = products.filter(category__name=category)
+        
     context = {
         "products": products,
     }
