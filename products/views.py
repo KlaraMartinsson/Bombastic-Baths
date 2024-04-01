9a66f5d084895acae10e400b885730c7960e4bd6
KlaraM <klara.ma@outlook.com>
2024-04-01 18:02:03 +0200

    Add code in all products view to make the items in a random order instead of just gifts after products

--- products/views.py
+++ products/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
+import random
 from .models import Product, Gift
 
 
 def all_products(request):
-    products = Product.objects.all()
+    products = list(Product.objects.all().order_by('?'))
+    gifts = list(Gift.objects.all().order_by('?'))
+
+    # Combine products and gifts into a single list
+    items = products + gifts
+
+    # Shuffle the combined list to randomize the order
+    random.shuffle(items)
 
     context = {
-        "products": products,
+        "items": items,
     }
-    return render (request, 'products/products.html', context)
+    return render(request, 'products/products.html', context)
+
 
 def all_bathbombs(request):
 
     bathbombs = Product.objects.all()
     category = None
+
     if 'category' in request.GET:
         category = request.GET['category']
         bathbombs = bathbombs.filter(category__name=category)
@@ -28,6 +38,7 @@ def all_gifts(request):
 
     gifts = Gift.objects.all()
     category = None
+    
     if 'category' in request.GET:
         category = request.GET['category']
         products = products.filter(category__name=category)
