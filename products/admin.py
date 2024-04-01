5935dcd72ddd02906990dcbfb4537adb8acff007
KlaraM <klara.ma@outlook.com>
2024-04-01 15:03:08 +0200

    Add model Gift and Occasion

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Occasion, Gift
 
 # Register your models here.
 
@@ -11,5 +11,15 @@ class ProductCategory(admin.ModelAdmin):
     list_display = ('name', 'friendly_name')
     search_fields = ['name', 'friendly_name']
 
+class GiftAdmin(admin.ModelAdmin):
+    list_display = ('sku', 'name', 'price','quantity')
+    search_fields = ['name']
+
+class OccasionCategory(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name')
+    search_fields = ['name', 'friendly_name']
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, ProductCategory)
+admin.site.register(Gift, GiftAdmin)
+admin.site.register(Occasion, OccasionCategory)
--- products/models.py
+++ products/models.py
@@ -31,3 +31,31 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Occasion(models.Model):
+    """
+    Model for gifts occasion
+    """
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Gift(models.Model):
+    """
+    Model for gifts
+    """
+    occasion = models.ForeignKey('Occasion', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
+    quantity = models.IntegerField(blank=False) 
+
+    def __str__(self):
+        return self.name
