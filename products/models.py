09c45e095713214ddee63c471af8264396dda3ab
KlaraM <klara.ma@outlook.com>
2024-04-04 00:51:28 +0200

    Add img field to gift model

--- products/models.py
+++ products/models.py
@@ -26,7 +26,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
@@ -53,6 +52,7 @@ class Gift(models.Model):
     occasion = models.ForeignKey('Occasion', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    image = models.ImageField(null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
     quantity = models.IntegerField(blank=False) 
