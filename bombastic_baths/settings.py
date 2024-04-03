5c448f40c0402a91f7833c4403e625efde0141aa
KlaraM <klara.ma@outlook.com>
2024-04-03 23:20:07 +0200

    Add cache control

--- bombastic_baths/settings.py
+++ bombastic_baths/settings.py
@@ -105,6 +105,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'af5387841714-bombastic-baths'
     AWS_S3_REGION_NAME = 'eu-north-1'
