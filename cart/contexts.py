5dfa9831c051f28dd7cd526859f13f243f125727
KlaraM <klara.ma@outlook.com>
2024-04-15 21:34:51 +0200

    Implement functionality for update quantity button

--- cart/contexts.py
+++ cart/contexts.py
@@ -15,11 +15,11 @@ def cart_contents(request):
 
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
+        total += int(quantity) * product.price
+        product_count += int(quantity)
         cart_items.append({
             'item_id': item_id,
-            'quantity': quantity,
+            'quantity': int(quantity),
             'product': product,
         })
 
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.contrib import messages
+from products.models import Product
 
 def shopping_cart(request):
     """A view that renders the shopping cart contents page"""
@@ -20,3 +23,21 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+def adjust_cart(request, item_id):
+    """ Updates cart """
+    cart = request.session.get('cart', {})
+    bathbomb = get_object_or_404(Product, pk=item_id)
+    quantity = request.POST.get('quantity')
+
+    if item_id in list(cart.keys()):
+        if int(quantity) > 0:
+            cart[item_id] = quantity
+            messages.success(
+                request, f'{bathbomb.name} quantity updated successfully.')
+        else:
+            messages.error(request, 'Something went wrong. Please try again.')
+
+    request.session['cart'] = cart
+
+    return redirect(reverse('cart'))
