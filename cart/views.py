8388e326848963319807f6c4b44d1cbd28db4c81
KlaraM <klara.ma@outlook.com>
2024-04-16 14:41:24 +0200

    Add functionality to remove items from the cart

--- cart/views.py
+++ cart/views.py
@@ -13,7 +13,6 @@ def add_to_cart(request, item_id):
     
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
@@ -25,7 +24,7 @@ def add_to_cart(request, item_id):
     return redirect(redirect_url)
 
 def adjust_cart(request, item_id):
-    """ Updates cart """
+    """Changes the quantity of products to the specified amount"""
     cart = request.session.get('cart', {})
     bathbomb = get_object_or_404(Product, pk=item_id)
     quantity = request.POST.get('quantity')
@@ -41,3 +40,20 @@ def adjust_cart(request, item_id):
     request.session['cart'] = cart
 
     return redirect(reverse('cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Removes items from shopping cart"""
+
+    cart = request.session.get('cart', {})
+    bathbomb = get_object_or_404(Product, pk=item_id)
+    if item_id in cart.keys():
+        try:
+            cart.pop(item_id)
+            messages.success(request, f'{bathbomb.name} removed from cart.')
+            request.session['cart'] = cart
+            return redirect(reverse('cart'))
+        except Exception as e:
+            messages.error(request, f'An error occured {e}')
+            return redirect(reverse('cart'))
+   
