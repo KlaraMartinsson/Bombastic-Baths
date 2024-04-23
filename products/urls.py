41e577a55be146a23c352fee6ba5892dcc780df3
KlaraM <klara.ma@outlook.com>
2024-04-23 18:44:06 +0200

    Complete webhook handler

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('bathbombs/', views.all_bathbombs, name='bathbombs'),
     path('gifts/', views.all_gifts, name='gifts'),
-    path('productinfo/<slug:slug>/', views.bathbombs_details, name='bathbomb-details'),
+    path('<slug:slug>/', views.bathbombs_details, name='bathbomb-details'),
     path('giftinfo/<slug:slug>/', views.gifts_details, name='gift-details'),
 ]
