cc06e84ce75f4f76b8bf1ea3991c3ae06c253865
KlaraM <klara.ma@outlook.com>
2024-04-27 16:28:19 +0200

    Add a function to clear all of the products in the users wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.view_wishlist, name='view_wishlist'),
     path('wishlist/add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('wishlist/remove/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path('wishlist/clear/<int:product_id>/', views.clear_wishlist, name='clear_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -38,10 +38,19 @@ def add_to_wishlist(request, product_id):
 
 @login_required
 def remove_from_wishlist(request, product_id):
-    """ A view to add products to users wishlist page """
+    """ A view to remove products from users wishlist page """
     product = get_object_or_404(Product, id=product_id)
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
     wishlist.products.remove(product)
     messages.success(request, f"{product.name} removed from your wishlist")
     wishlist.save()
+    return redirect('view_wishlist')
+
+@login_required
+def clear_wishlist(request, product_id):
+    """ A view to clear all products from users wishlist page """
+    product = get_object_or_404(Product, id=product_id)
+    wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+    wishlist.products.clear()
+    messages.success(request, "Your wishlist has been cleared")
     return redirect('view_wishlist')
